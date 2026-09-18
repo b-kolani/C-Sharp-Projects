be09b3667f6fb60bbf4181bb6fe6cf7c06ed3967
DanameKOLANI <61797847+DanameKOLANI@users.noreply.github.com>
2026-09-19 00:54:37 +0100

    first commit

---
+++ TaskManager/Program.cs
@@ -0,0 +1,40 @@
+﻿
+List<TaskItem>  tasks = new List<TaskItem>();
+
+tasks.Add(
+    new TaskItem(
+        "Learn C#", 
+        "Learning C# through hands-on project"
+    )
+);
+
+tasks.Add(
+    new TaskItem(
+        "Learn ASP.Net Core", 
+        "Learning ASP.Net Core through hands-on project"
+    )
+);
+
+tasks.Add(
+    new TaskItem(
+        "Learn Angular", 
+        "Learning Angular through hands-on project"
+    )
+);
+
+int id = 1;
+
+foreach (TaskItem task in tasks)
+{
+    task.Id = id;
+    id++;
+}
+
+TaskItem? foundTask = tasks.FirstOrDefault(task => task.Id == 2);
+
+Console.WriteLine(
+    "Id: " + foundTask?.Id + "\n" + 
+    "Title: " + foundTask?.Title + "\n" + 
+    "Description: " + foundTask?.Description + "\n" +
+    "Created: " + foundTask?.CreatedAt + "\n"
+);
